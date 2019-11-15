812bf026ea44bab6c1d2d3cc2e8422c843f2e8a0
Hector Jeronimo Cuellar Villalobos <hectorcuellar@MacBook-Pro.local>
2019-11-14 20:48:10 -0600

    update dependencies

--- server/server.js
+++ server/server.js
@@ -20,7 +20,9 @@ app.use(require('./routes/index'));
 
 
 
-mongoose.connect(process.env.URLDB, (err, res) => {
+mongoose.connect(process.env.URLDB,
+    {useNewUrlParser: true, useCreateIndex: true}, 
+    (err, res) => {
 
     if (err) throw err;
 
